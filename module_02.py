ada75314f5b6ac089a6c31db4b7136f406349ba7
Czedrick <cmarcelino2@rrc.ca>
2024-09-10 00:19:01 -0500

    Initial commit. Documented Project.

---
+++ module_02.py
@@ -0,0 +1,14 @@
+"""
+Description: Module 02 demonstration
+Author: Czedrick Marcelino
+Date: September 9, 2024
+Usage: To demsontarte content from Module 02.
+"""
+
+# This is a single-line comment.
+
+"""
+This is a multi-line comment.
+
+It can span multiple lines.
+"""
